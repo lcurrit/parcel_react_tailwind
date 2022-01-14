7d0f4054f695de63d22aaea9c944687e0ea483a6
Levi Currit <Levi.Currit@tivityhealth.com>
2022-01-14 11:50:17 -0700

    Husky Test 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-/* Husky Test */
-var foo = bar;
-
 export default function App() {
   return (
     <div className="container mx-auto px-4 lg:max-w-5xl h-full flex flex-col">
