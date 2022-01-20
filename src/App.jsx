71250949fc427d3b994d64ea08935f648cbfd80c
Levi Currit <Levi.Currit@tivityhealth.com>
2022-01-20 11:28:18 -0700

    SCSS Testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import styles from "./App.module.scss";
 
 export default function App() {
   return (
-    <div className="container mx-auto px-4 lg:max-w-5xl h-full flex flex-col">
-      <h1>Hello World!</h1>
-      <p className={styles.subtitle}>How you doin&#39;?</p>
+    <div className={styles.content}>
+      <h1 className={styles.teal}>Hello World!</h1>
     </div>
   );
 }
