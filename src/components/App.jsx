8f5e1d992d72534e29465f49546f6630a2d7f5c6
Levi Currit <Levi.Currit@tivityhealth.com>
2022-01-21 13:27:08 -0700

    Lint-Staged Test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import HelloWorld from "./HelloWorld";
 
 export default function App() {
+  // eslint-disable-next-line no-undef
+  if (process.env.NODE_ENV === "development") {
+    console.log("Happy developing!");
+  }
   return (
     <div className="container mx-auto px-4 lg:max-w-5xl h-full flex flex-col">
       <HelloWorld />
