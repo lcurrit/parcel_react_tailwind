2ee67cca6018857094aadce06d4179a670f882b5
Levi Currit <Levi.Currit@tivityhealth.com>
2022-01-24 14:56:11 -0700

    TypeScript Example

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import * as styles from "./HelloWorld.module.css";
 
-export default function HelloWorld() {
-  return <h1 className={styles.decoration}>Hello World!</h1>;
+interface HelloWorldProps {
+  name: string;
+}
+
+HelloWorld.defaultProps = {
+  name: "World",
+};
+
+export default function HelloWorld(props: HelloWorldProps) {
+  return <h1 className={styles.decoration}>Hello {props.name}!</h1>;
 }
