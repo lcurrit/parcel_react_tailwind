84d598ab0abf8e0dd678ccb65ae5919d439809a3
Levi Currit <Levi.Currit@tivityhealth.com>
2022-01-24 14:57:46 -0700

    Remove Console Log

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-console.log("whatever");
-
 const app = document.getElementById("app");
 ReactDOM.render(<App />, app);
