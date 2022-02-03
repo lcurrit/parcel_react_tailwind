e1e34f5188e7f29c0b153531f8e51181fe341f4b
Levi Currit <Levi.Currit@tivityhealth.com>
2022-02-03 09:40:00 -0700

    Synced Changes

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  app
+);
